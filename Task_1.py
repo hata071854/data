7bbbed5443b05fa3e29245310de634771d3a388c
hata071854 <konkow85@mail.ru>
2025-01-11 19:18:52 +0300

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,23 @@
+# '1 st program'
+
+print(9 ** 0.5 * 5)
+
+# '2 st program'
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# '3 st program'
+
+a = 1234
+
+b = 5678
+
+c = a // 10 % 100
+
+d = b // 10 % 100
+
+print(c + d)
+
+# '4 st program'
+
+print(int(13.42) == 42.13 * 100 % 100 or 42.13 * 100 % 100 == int(42.13))
